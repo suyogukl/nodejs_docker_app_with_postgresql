fbcdd7a0c15b8d52af879847c0d1a1d3f685a00b
Suyog Ukalkar <suyogu@thoughtworks.com>
2019-02-06 15:01:06 +0530

    change url path in files

--- app.js
+++ app.js
@@ -1,44 +1,43 @@
-const app = require('express')();
-const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
 const PORT = process.env.PORT || 8000;
 let connectionString = process.env.DATABASE_URL || "postgres://localhost:5432/postgres";
 
 const { Client } = require('pg');
 const client = new Client(connectionString);
 
-console.log(connectionString);
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}));
 
-app.use(bodyParser());
-
-app.get('/allNumbers', (req, res) => {
-    client.query('SELECT numbers from numbers', null, (err, result) => {
+app.get('/user', (req, res) => {
+    client.query('SELECT * from users', null, (err, result) => {
         if(err) {
             res.status(500);
             res.send(err.message);
             return;
         }
-        res.json({ "numbers": result.rows });    
+        res.json({ "users": result.rows });   
     });
     
 });
 
-app.post('/saveNumber', (req, res) => {
-    let number = req.body.number;
-    client.query(`insert into numbers values('${number}')`,null,(err,number) => {
+app.post('/', (req, res) => {
+    let user = req.body.username;
+    client.query(`insert into users values('${user}')`,null,(err,user) => {
         if(err) {
             res.status(500);
             res.end();
         } else {
-            res.status(201);
+            res.status(200);
+            console.log('aala re');
             res.end();
-        }
-        
+        }  
     });
 });
 
 client.connect()
 .then(function(){
-    client.query('CREATE TABLE IF NOT EXISTS numbers(numbers integer);',(err,result) => {
+    client.query('CREATE TABLE IF NOT EXISTS users( username varchar(10))',(err,result) => {
         if(err) {
             console.log(`Error occur while creating table ${err.message}`);
         } else {
